f06d773d939b70d7c177a258e469f12bf834b537
Nick Chen <nickchenchj@gmail.com>
2020-10-14 21:10:43 +0800

    Implement csv-to-json converter (single thread)

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CC := gcc
-CFLAGS := -Wall -pthread -O3
+CFLAGS := -Wall -pthread -O3 -std=c99
+EXE := convert
 
 all:
+	$(CC) $(CFLAGS) -o $(EXE) main.c
 	$(CC) $(CFLAGS) -o generator csv_generator.c
 
 .PHONY: clean
+
 clean:
-	rm -f generator
+	rm -f $(EXE) generator
---
+++ main.c
@@ -0,0 +1,68 @@
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE (13 * 20)
+#define INT_PER_LINE 20
+
+static FILE *fp_in, *fp_out;
+static int32_t total_threads = 1;
+
+int main(int argc, char *argv[]) {
+    const char input_filename[] = "input.csv";
+    const char output_filename[] = "output.json";
+
+    if (argc > 2) {
+        puts("Error: too many arguments");
+        printf("Usage: %s [number-of-threads]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    } else if (argc == 2)
+        total_threads = atoi(argv[1]);
+
+    pthread_t *t = malloc(total_threads * sizeof(pthread_t));
+
+    /* Open input file */
+    if (!(fp_in = fopen(input_filename, "r"))) {
+        printf("Error: unable to open %s\n", input_filename);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Open output file */
+    if (!(fp_out = fopen(output_filename, "w"))) {
+        printf("Error: unable to open %s\n", input_filename);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(fp_out, "[\n");
+
+    int32_t n[INT_PER_LINE];
+    uint64_t total_lines = 0;
+    /* TODO:
+     * Analyze the efficiency for both fscanf and fgets */
+    while (fscanf(fp_in,
+                  "%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d",
+                  &n[0], &n[1], &n[2], &n[3], &n[4], &n[5], &n[6], &n[7], &n[8],
+                  &n[9], &n[10], &n[11], &n[12], &n[13], &n[14], &n[15], &n[16],
+                  &n[17], &n[18], &n[19]) == INT_PER_LINE) {
+        total_lines++;
+        if (total_lines == 1)
+            fprintf(fp_out, "\t{\n");
+        else
+            fprintf(fp_out, ",\n\t{\n");
+
+        for (uint8_t i = 0; i < INT_PER_LINE; i++) {
+            if (i == INT_PER_LINE - 1)
+                fprintf(fp_out, "\t\t\"col_%d\":%d\n", i + 1, n[i]);
+            else
+                fprintf(fp_out, "\t\t\"col_%d\":%d,\n", i + 1, n[i]);
+        }
+        fprintf(fp_out, "\t}");
+    }
+    fprintf(fp_out, "\n]");
+
+    free(t);
+    fclose(fp_in);
+    fclose(fp_out);
+
+    return 0;
+}
