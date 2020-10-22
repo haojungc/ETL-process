28efce6f89d0fb83e5e9e7119565e1d9596a6e56
Nick Chen <nickchenchj@gmail.com>
2020-10-23 01:29:18 +0800

    Fix warnings

--- csv_generator.c
+++ csv_generator.c
@@ -11,12 +11,12 @@
 #define BUF_PER_INT 13
 #define MAX_BUF_SIZE (INT_PER_LINE * BUF_PER_INT)
 
-static void write_data(uint64_t lines);
+static void write_data(uint32_t lines);
 static void *write_data_parallel(void *thread_id);
 static int32_t rand_int32();
 
 static FILE *fp;
-static uint64_t total_lines = 2e7;
+static uint32_t total_lines = 5000000;
 static uint32_t total_threads = 1;
 
 int main(int argc, char *argv[]) {
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
         }
     case 2:
         /* Invalid format */
-        if (sscanf(argv[1], "--line=%lu", &total_lines) != 1) {
+        if (sscanf(argv[1], "--line=%u", &total_lines) != 1) {
             puts("Error: invalid format");
             printf("Usage: %s [--line=<number-of-lines>] "
                    "[--thread=<number-of-threads>]\n",
@@ -59,10 +59,10 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
         /* Invalid value */
-        if (!total_lines || total_lines >> 63 == 1) {
+        if (!total_lines || total_lines >> 31 == 1) {
             printf("Error: invalid value for --line, should be between 1 and "
-                   "%ld\n",
-                   INT64_MAX);
+                   "%d\n",
+                   INT32_MAX);
             printf("Usage: %s [--line=<number-of-lines>] "
                    "[--thread=<number-of-threads>]\n",
                    argv[0]);
@@ -85,8 +85,7 @@ int main(int argc, char *argv[]) {
         printf("Warning: %s already exists\n", filename);
         puts("Continue? (y: overwrite, n: abort)");
         char c;
-        scanf("%c", &c);
-        if ((c | ' ') != 'y')
+        if (scanf("%c", &c) == 1 && (c | ' ') != 'y')
             exit(EXIT_FAILURE);
     }
 
@@ -95,7 +94,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    printf("Writing %lu lines of data to %s using %d thread(s) ...\n",
+    printf("Writing %u lines of data to %s using %d thread(s) ...\n",
            total_lines, filename, total_threads);
 
     clock_t start, end;
@@ -105,10 +104,16 @@ int main(int argc, char *argv[]) {
         write_data(total_lines);
     else {
         /* Writes data to output file using pthread */
-        pthread_t *t = malloc(total_threads * sizeof(pthread_t));
+        pthread_t *t = (pthread_t *)malloc(total_threads * sizeof(pthread_t));
+        uint32_t *tid = (uint32_t *)malloc(total_threads * sizeof(uint32_t));
+        if (!t) {
+            perror("malloc failed");
+            exit(EXIT_FAILURE);
+        }
         for (uint32_t i = 0; i < total_threads; i++) {
-            int32_t stat =
-                pthread_create(&t[i], NULL, write_data_parallel, (void *)i);
+            tid[i] = i;
+            int32_t stat = pthread_create(&t[i], NULL, write_data_parallel,
+                                          (void *)&tid[i]);
             if (stat) {
                 printf("ERROR: return code from pthread_create() is %d\n",
                        stat);
@@ -119,20 +124,21 @@ int main(int argc, char *argv[]) {
             pthread_join(t[i], NULL);
 
         free(t);
+        free(tid);
     }
     fclose(fp);
     end = clock();
 
     puts("Done.");
-    printf("-----\nElapsed Time: %.2lf secs\n-----\n",
+    printf("Elapsed Time: %.2lf secs\n",
            (double)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
 
-static void write_data(uint64_t lines) {
+static void write_data(uint32_t lines) {
     int32_t n[INT_PER_LINE];
-    for (uint64_t i = 0; i < lines; i++) {
+    for (uint32_t i = 0; i < lines; i++) {
         for (uint32_t j = 0; j < INT_PER_LINE; j++) {
             n[j] = rand_int32();
         }
@@ -144,10 +150,10 @@ static void write_data(uint64_t lines) {
 }
 
 static void *write_data_parallel(void *thread_id) {
-    uint32_t tid = (uint32_t)thread_id;
+    uint32_t tid = *(uint32_t *)thread_id;
     printf("  Starting thread %d ...\n", tid);
-    uint64_t chuck_size = total_lines / total_threads;
-    uint64_t lines = ((tid + 1) * chuck_size > total_lines)
+    uint32_t chuck_size = total_lines / total_threads;
+    uint32_t lines = ((tid + 1) * chuck_size > total_lines)
                          ? total_lines % chuck_size
                          : chuck_size;
     write_data(lines);
